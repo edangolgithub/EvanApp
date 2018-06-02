599e1c6ab44530d292b4f4b4c73911854455f803
edangol <dangolevan@gmail.com>
2018-06-02 14:23:25 +0545

    fix bugs

--- EvanApp/EvanApp.Android/Pages/NumberGuessingGamePage.xaml.cs
+++ EvanApp/EvanApp.Android/Pages/NumberGuessingGamePage.xaml.cs
@@ -38,55 +38,65 @@ namespace EvanApp.Droid.Pages
 
         private void Restartbtn_Clicked(object sender, EventArgs e)
         {
+            Restart();
+        }
+
+        private void Restart()
+        {
             r = new Random();
             int value = r.Next(1, 100);
             memory = value;
             infotxt.Text = "";
-            chance = 8;
+            numtxt.Text = "";
+            kk.Text = "";
+            chance = 7;
         }
 
         private void Submitbtn_Clicked(object sender, EventArgs e)
         {
+            infotxt.Text = "";
             if (numtxt.Text.Length < 1)
             {
                 DisplayAlert("Warning", "You did not enter any thing", "ok");
                 return;
             }
-            infotxt.Text = "";
+            
             if (!int.TryParse(numtxt.Text, out num))
             {
-                DisplayAlert("error", "That doesnt look like a number", "ok");
+                DisplayAlert("Error", "That doesnt look like a number", "ok");
                 return;
             }
 
-
-            if (chance <= 1)
-            {
-                infotxt.Text = "you are not good player \nyou lost!!!!!" +
-                 "\nthe number was " + memory;
-                return;
-            }
 
             chance--;
 
+            kk.Text = "You entered " + numtxt.Text;
             if (num < memory)
             {
-                infotxt.Text = "too low" + " you have " + (chance) + " chance left";
+                infotxt.Text = "Too Low" + " you have " + (chance) + " chance(s) left";
             }
             else if (num > memory)
             {
-                infotxt.Text = "too high" + " you have " + (chance) + " chance left";
+                infotxt.Text = "Too High" + " you have " + (chance) + " chance(s) left";
             }
             else
             {
-                infotxt.Text = "correct answer"
+                infotxt.Text = "Correct answer"
                   + "\nyou are very good player \n congratulation!!!!!";
 
             }
-
+            
             numtxt.Text = "";
+            if (chance <= 0)
+            {
+                DisplayAlert("Looser", "you are not good player \nyou lost!!!!!" +
+                   "\nthe number was " + memory, "Ok");
+                Restart();
+                return;
+            }
 
-         
+
+
 
         }
     }
